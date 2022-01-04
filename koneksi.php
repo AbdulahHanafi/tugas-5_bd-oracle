6a28b43c2df5c6ab5871b6ee737cc30304631fb8
AbdulahHanafi <45525482+AbdulahHanafi@users.noreply.github.com>
2022-01-04 22:16:31 +0700

    Menampilkan-join-mahasiswa
    1. Menampilakn hasil koneksi dari oracle ke bahasa pemrograman
    2. Menampilkan view dalam database masing-masing
    3. Menampilkan Halaman Home/Beranda
    4. Menampilkan jumlah data dari view menggunakan query count
    5. Menampilkan data untuk master data (Admin,buku,mahsiswa)

---
+++ koneksi.php
@@ -0,0 +1,22 @@
+<?php 
+ 
+//membangun koneksi 
+$username="ABDUL_100";
+$password="100";
+$database="LOCALHOST/XE";
+
+$koneksi=oci_connect($username,$password,$database);
+
+$hari_ini = date("dmY");
+
+if (!$koneksi) { 
+$erroci_error ();
+
+echo "Gagal tersambung ke ORACLE";
+
+} else{
+//echo "koneksi berhasil";
+
+}
+
+?>
